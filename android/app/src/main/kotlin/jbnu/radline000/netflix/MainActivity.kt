84f7b1399f1587a7007604b7f38143e5ea3ba183
Radline000 <radline000@gmail.com>
2020-11-26 17:33:37 +0900

    first commit

---
+++ android/app/src/main/kotlin/jbnu/radline000/netflix/MainActivity.kt
@@ -0,0 +1,6 @@
+package jbnu.radline000.netflix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:netflix/screen/home_screen.dart';
+import 'package:netflix/widget/bottom_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // This widget is the root of your application.
+  TabController controller;
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        accentColor: Colors.white,
+      ),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: [
+              HomeScreen(),
+              Container(child: Center(child: Text('search'),),),
+              Container(child: Center(child: Text('save_alt'),),),
+              Container(child: Center(child: Text('list'),),),
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
+        ),
+      ),
+    );
+  }
+}
